4826b925fbd0d4fc588b8e9a51bc7977a842e4ae
abdul <abdul.tanveer@gmail.com>
2019-08-22 19:29:28 +0530

    fragments-- handling listview onitemclicklistener

--- app/src/main/java/com/example/cgiday3app/HeadlinesFragment.java
+++ app/src/main/java/com/example/cgiday3app/HeadlinesFragment.java
@@ -8,13 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HeadlinesFragment extends Fragment {
+public class HeadlinesFragment extends Fragment implements AdapterView.OnItemClickListener {
 
+    ListView headlinesListView;
 
     public HeadlinesFragment() {
         // Required empty public constructor
@@ -25,7 +29,17 @@ public class HeadlinesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false);
+        View view = inflater.inflate(R.layout.fragment_headlines, container, false);
+    headlinesListView = view.findViewById(R.id.headlineslv);
+    headlinesListView.setOnItemClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        String itemClicked = adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(getContext(), itemClicked, Toast.LENGTH_SHORT).show();
+
+    }
 }
